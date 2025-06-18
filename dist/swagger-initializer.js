060def4814bd3a19ec1eeffed92e4456e92c90ca
Abezluckiy <a.bezluckiy@dunice.net>
2025-06-18 14:06:38 +0300

    Add base server yaml file

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,10 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    urls: [
+    { url :"advanced_server.yaml", name : "Advanced server"},
+    { url : "base_server.yaml", name : "Base server"}
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
